0454caa046a3096588fe56280fea1dd6f969265a
vasu <abc@gmail.com>
2022-03-23 15:54:21 +0000

    hello.java pom.xml created

---
+++ hello.java
@@ -0,0 +1,9 @@
+/*
+* The HelloWorld Java program
+* prints "Hello World!" on the computer screen
+*/
+class HelloWorld {
+  public static void main(String[] args) {
+   System.out.println("Hello, World!");
+  }
+}
